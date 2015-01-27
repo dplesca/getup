48afd0f7414446f2d08521d8a5c181ae2ea27e9e
Dragos Plesca <dragos.plesca@gmail.com>
2015-01-27 22:55:37 +0200

    simplified js

--- app.js
+++ app.js
@@ -4,33 +4,29 @@ $(function(){
 	$('.start button').click(function(ev){
 		$('.start').toggleClass('hidden');
 		$(".example").TimeCircles({
-  		"animation": "ticks",
-  		"count_past_zero": false,
-  		"circle_bg_color": "#fff",
-  		"time": {
-  			Days: {
-                show: false,
-                text: "Days",
-                color: "#FC6"
-            },
-            Hours: {
-                show: false,
-                text: "Hours",
-                color: "#9CF"
-            },
-            Minutes: {
-                color: "#1abc9c"
-            },
-            Seconds: {
-                color: "#e74c3c"
-            }
-  		}
-  	}).addListener(function(unit, value, total){
-  		if(!total){
-		    yourSound.play();
-		    $('.stop').toggleClass('hidden');
-  		}
-  	});
+	  		"animation": "ticks",
+	  		"count_past_zero": false,
+	  		"circle_bg_color": "#fff",
+	  		"time": {
+	  			Days: {
+	                show: false
+	            },
+	            Hours: {
+	                show: false
+	            },
+	            Minutes: {
+	                color: "#1abc9c"
+	            },
+	            Seconds: {
+	                color: "#e74c3c"
+	            }
+	  		}
+	  	}).addListener(function(unit, value, total){
+	  		if(!total){
+			    yourSound.play();
+			    $('.stop').toggleClass('hidden');
+	  		}
+	  	});
 	});
   	
 	$('.stop button').click(function(ev){
